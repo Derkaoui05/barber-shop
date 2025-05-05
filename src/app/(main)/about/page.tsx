c3203910b640a7ca6c8fc9b8feefd8ff3a973049
Derkaoui05 <147924188+Derkaoui05@users.noreply.github.com>
2025-05-05 18:52:20 +0100

    some content in about page

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -1,10 +1,33 @@
+import Image from "next/image";
+import React from "react";
+import { Button } from "@/components/ui/button";
+
 export default function AboutPage() {
   return (
-    <main className="flex-1">
-      <div className="container py-16">
-        <h1 className="text-4xl font-bold mb-8">About Us</h1>
-        {/* Add your about content here */}
+    <section className="font-oswald w-full py-12 md:py-16">
+      <div className="container mx-auto px-4 md:px-8">
+        <div className="flex flex-col items-center justify-center gap-8 max-w-6xl mx-auto">
+          <div className="flex-1 space-y-4 w-full">
+            <div>
+              <h2 className="text-6xl md:text-7xl tracking-wide leading-relaxed font-bold">
+                An Award Winning Barbershop
+              </h2>
+              <div className="relative w-full aspect-[21/9]">
+                <Image
+                  src="/about2.webp"
+                  alt="about image"
+                  fill
+                  priority
+                  className="object-cover rounded-lg"
+                />
+              </div>
+                <h2 className="text-6xl md:text-7xl tracking-wide leading-relaxed font-bold">
+                  Built By Amazing Barbers.
+                </h2>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+    </section>
   );
 }

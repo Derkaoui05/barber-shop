6f461afa6727f7f618679a8832356e047a3ab30f
Derkaoui05 <147924188+Derkaoui05@users.noreply.github.com>
2026-01-10 18:12:37 +0100

    fix: images by using Image/next

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -1,60 +1,62 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
 
 function Testimonials() {
   return (
     <div>
-      <div className='testimonial'>
-        <div className='testimonial__container container grid'>
-          <div className='testimonial__data'>
-            <h2 className='section__title'>Testimonials</h2>
-            <p className='section__subtitle'>What my clients say about me</p>
+      <div className="testimonial">
+        <div className="testimonial__container container grid">
+          <div className="testimonial__data">
+            <h2 className="section__title">Testimonials</h2>
+            <p className="section__subtitle">What my clients say about me</p>
           </div>
-          <div className='testimonial__content grid'>
-            <div className='testimonial__card'>
-              <div className='testimonial__header'>
-                <Image src='assets/img/testimonial-1.jpg' alt='kkkkkkk' width={200} height={200} className='testimonial__img' />
+          <div className="testimonial__content grid">
+            <div className="testimonial__card">
+              <div className="testimonial__header">
+                <Image
+                  src="assets/img/testimonial-1.jpg"
+                  alt="kkkkkkk"
+                  width={200}
+                  height={200}
+                  className="testimonial__img"
+                />
                 <div>
-                  <h3 className='testimonial__name'>John Doe</h3>
-                  <span className='testimonial__profession'>Frontend Developer</span>
+                  <h3 className="testimonial__name">John Doe</h3>
+                  <span className="testimonial__profession">Frontend Developer</span>
                 </div>
               </div>
-              <p className='testimonial__description'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatibus, sapiente.
+              <p className="testimonial__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, sapiente.
               </p>
             </div>
-            <div className='testimonial__card'>
-              <div className='testimonial__header'>
-                <img src='assets/img/testimonial-2.jpg' alt='' className='testimonial__img' />
+            <div className="testimonial__card">
+              <div className="testimonial__header">
+                <Image src="assets/img/testimonial-2.jpg" alt="" className="testimonial__img" />
                 <div>
-                  <h3 className='testimonial__name'>Jane Doe</h3>
-                  <span className='testimonial__profession'>Backend Developer</span>
+                  <h3 className="testimonial__name">Jane Doe</h3>
+                  <span className="testimonial__profession">Backend Developer</span>
                 </div>
               </div>
-              <p className='testimonial__description'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatibus, sapiente.
+              <p className="testimonial__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, sapiente.
               </p>
             </div>
-            <div className='testimonial__card'>
-              <div className='testimonial__header'>
-                <img src='assets/img/testimonial-3.jpg' alt='' className='testimonial__img' />
+            <div className="testimonial__card">
+              <div className="testimonial__header">
+                <img src="assets/img/testimonial-3.jpg" alt="" className="testimonial__img" />
                 <div>
-                  <h3 className='testimonial__name'>John Smith</h3>
-                  <span className='testimonial__profession'>Fullstack Developer</span>
+                  <h3 className="testimonial__name">John Smith</h3>
+                  <span className="testimonial__profession">Fullstack Developer</span>
                 </div>
               </div>
-              <p className='testimonial__description'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatibus, sapiente.
+              <p className="testimonial__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, sapiente.
               </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Testimonials
+export default Testimonials;
